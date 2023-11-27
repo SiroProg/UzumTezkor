5a03b88f88c7201c92fe7735f07b046d9cab816b
username <you@example.com>
2023-11-27 17:29:39 +0500

    basket page updated

--- lib/src/common/model/location/place_location.dart
+++ lib/src/common/model/location/place_location.dart
@@ -6,11 +6,19 @@ class PlaceLocation {
   final double latitude;
   final String address;
   final bool isSelected;
+  final String podezd;
+  final String floor;
+  final String flat;
+  final String comment;
 
   PlaceLocation({
     required this.longitude,
     required this.latitude,
     required this.address,
+    this.podezd = '',
+    this.floor = '',
+    this.flat = '',
+    this.comment = '',
     this.isSelected = true,
   }) : id = uuid.v4();
 
@@ -19,12 +27,20 @@ class PlaceLocation {
     double? latitude,
     String? address,
     bool? isSelected,
+    String? podezd,
+    String? floor,
+    String? flat,
+    String? comment,
   }) {
     return PlaceLocation(
       longitude: longitude ?? this.longitude,
       latitude: latitude ?? this.latitude,
       address: address ?? this.address,
       isSelected: isSelected ?? this.isSelected,
+      podezd: podezd ?? this.podezd,
+      floor: floor ?? this.floor,
+      flat: flat ?? this.flat,
+      comment: comment ?? this.comment,
     );
   }
 
@@ -33,26 +49,41 @@ class PlaceLocation {
       identical(this, other) ||
       other is PlaceLocation &&
           runtimeType == other.runtimeType &&
+          id == other.id &&
           longitude == other.longitude &&
           latitude == other.latitude &&
           address == other.address &&
-          isSelected == other.isSelected;
+          isSelected == other.isSelected &&
+          podezd == other.podezd &&
+          floor == other.floor &&
+          flat == other.flat &&
+          comment == other.comment;
 
   @override
   int get hashCode =>
+      id.hashCode ^
       longitude.hashCode ^
       latitude.hashCode ^
       address.hashCode ^
-      isSelected.hashCode;
+      isSelected.hashCode ^
+      podezd.hashCode ^
+      floor.hashCode ^
+      flat.hashCode ^
+      comment.hashCode;
 
   @override
   String toString() {
     return 'PlaceLocation'
         '{'
+        'id: $id, '
         'longitude: $longitude, '
         'latitude: $latitude, '
         'address: $address, '
-        'isSelected: $isSelected'
+        'isSelected: $isSelected, '
+        'podezd: $podezd, '
+        'floor: $floor, '
+        'flat: $flat, '
+        'comment: $comment'
         '}';
   }
 }
